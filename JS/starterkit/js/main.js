fe90b805c83e424439f1d1da137b994abafc50f6
Sylvain Delafoy <sylvain.delafoy@laposte.net>
2018-11-15 01:43:53 +0100

    solution en javascript

--- JS/starterkit/js/main.js
+++ JS/starterkit/js/main.js
@@ -1,6 +1,31 @@
 $("document").ready(function() {
-  // Your javascript goes here...
-  
-  
-  
+  function act() {
+    let decoded = "";
+    const message = $("#message")
+      .val()
+      .toLowerCase();
+    const source = $("#alphabetsource")
+      .val()
+      .toLowerCase()
+      .split("\n");
+    const dest = $("#alphabetdestination")
+      .val()
+      .toLowerCase()
+      .split("\n");
+    for (var i = 0, keyidx=0; i < message.length; i++) {
+      console.log(dest[i % dest.length], dest);
+      const idx = message.charAt(i);
+      if (source[i % source.length].includes(idx)) {
+        decoded +=
+          dest[keyidx % dest.length][source[keyidx % source.length].indexOf(idx)];
+          keyidx++;
+      } else {
+        decoded += idx;
+      }
+    }
+    $("#result").text(decoded);
+  }
+  $("#alphabetsource").change(act);
+  $("#alphabetdestination").change(act);
+  $("#message").change(act);
 });
